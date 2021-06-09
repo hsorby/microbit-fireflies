4edaa32fa3e3a46460ff7568eccbeaca44433024
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-06-10 09:22:54 +1200

    Implement fireflies code.

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+radio.onReceivedString(function (receivedString) {
+    if (clock < 8) {
+        clock += 1
+    }
+})
+let clock = 0
+radio.setGroup(8)
+radio.setTransmitPower(1)
+basic.forever(function () {
+    if (clock >= 8) {
+        radio.sendNumber(0)
+        game.addScore(1)
+        basic.pause(200)
+        clock = 0
+    } else {
+        basic.pause(100)
+        clock += 1
+    }
+})
